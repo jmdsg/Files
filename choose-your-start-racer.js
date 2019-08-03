b5e90f7a7bdeccd393f17840224665c0793b2f2e
jmdsg <jose.mdsg@gmail.com>
2019-08-02 21:18:56 -0400

    Added choose your start file

---
+++ choose-your-start-racer.js
@@ -0,0 +1,37 @@
+
+document.addEventListener("load", function() {
+
+	setTimeout(function() {
+
+		const container = document.getElementById("edit-choice-63");
+		const children = Array.from(container.children);
+		const data = children.map(div => ({
+			input: div.getElementsByTagName("input")[0],
+			name: div.getElementsByTagName("label")[0].textContent,
+		}));
+		const processName = str => str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
+		const targetName = "Claudio Nóbrega";
+		const target = data.filter(({ name }) => processName(name).indexOf(processName(targetName)) >= 0)[0];
+
+		const { input } = target;
+		const submit = document.getElementById("edit-submit-advpoll-2895");
+		const form = document.getElementById("advpoll-form-2895");
+
+		input.click();
+		submit.dispatchEvent(new Event("mousedown"));
+
+		setTimeout(function() {
+
+			﻿document.cookie = "_ga=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=.ralivm.com; path=/;"
+			document.cookie = "_gid=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=.ralivm.com; path=/;"
+			document.cookie = "_gat=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=.ralivm.com; path=/;"
+			document.cookie = "advpoll2895=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=.ralivm.com; path=/;"
+			document.cookie = "DRUPAL_UID=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=.ralivm.com; path=/;"
+
+			﻿location.reload();
+
+		}, 200);
+
+	}, 200);
+
+});
